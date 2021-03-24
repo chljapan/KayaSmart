d26705ff0d1a487513dc97d7ea1c514e700af71b
LiangChen <LiangChen@DESKTOP-8ED6TEN.mshome.net>
2021-03-24 11:27:53 +0800

    update BaseService

--- src/main/java/com/smartkaya/service/KayaBaseService.java
+++ src/main/java/com/smartkaya/service/KayaBaseService.java
@@ -1,12 +1,26 @@
 package com.smartkaya.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.smartkaya.bean.Paramater;
 import com.smartkaya.bean.Paramaters;
 
 public abstract class KayaBaseService {
 	private Paramaters paramaters;
+	private List<Paramaters> paramatersList;
+	private Paramater paramater;
+	private List<Paramater> paramaterList;
+	
 	//这个参数是各个基本方法执行的顺序
 	public static KayaBaseService excuteService(Paramaters paramaters) {
 		return KayaFactory.createKayaService(paramaters);
+	}
+	public static KayaBaseService excuteService(String serviceName, ArrayList<Paramaters> paramatersList) {
+		return KayaFactory.createKayaService(serviceName, paramatersList);
+	}
+	public static KayaBaseService excuteService(Paramater paramater) {
+		return KayaFactory.createKayaService(paramater);
 	}
 	
 	public void setParamaters(Paramaters paramaters) {
@@ -17,6 +31,30 @@ public abstract class KayaBaseService {
 		return paramaters;
 	}
 	
+	public List<Paramaters> getParamatersList() {
+		return paramatersList;
+	}
+
+	public void setParamatersList(List<Paramaters> paramatersList) {
+		this.paramatersList = paramatersList;
+	}
+
+	public Paramater getParamater() {
+		return paramater;
+	}
+
+	public void setParamater(Paramater paramater) {
+		this.paramater = paramater;
+	}
+
+	public List<Paramater> getParamaterList() {
+		return paramaterList;
+	}
+
+	public void setParamaterList(List<Paramater> paramaterList) {
+		this.paramaterList = paramaterList;
+	}
+
 	public abstract void before();
 	
 	public void excutBusiness(){
--- src/main/java/com/smartkaya/service/KayaFactory.java
+++ src/main/java/com/smartkaya/service/KayaFactory.java
@@ -1,5 +1,8 @@
 package com.smartkaya.service;
 
+import java.util.ArrayList;
+
+import com.smartkaya.bean.Paramater;
 import com.smartkaya.bean.Paramaters;
 import com.smartkaya.log.KayaLogManager;
 
@@ -24,4 +27,62 @@ public class KayaFactory {
 		}
 		return kayaBaseService;
 	}
+
+	
+	public static KayaBaseService createKayaService(String serviceName,ArrayList<Paramaters> paramatersList){
+		KayaBaseService kayaBaseService=null; //定义一个类
+		try {
+
+			kayaBaseService = (KayaBaseService)Class.forName(serviceName).newInstance(); //TODO:Packagepath 需要在参数中动态提取
+			kayaBaseService.setParamatersList(paramatersList);
+			kayaBaseService.operate();
+			
+		} catch (InstantiationException e) {//必须指定服务类
+			kayaLoger.error("The service class must be specified!");
+		} catch (IllegalAccessException e) { //服务类定义错误
+
+			kayaLoger.error("The service class is incorrectly defined!");
+		} catch (ClassNotFoundException e) { //找不到服务类
+			kayaLoger.error("The service class could not be found!");
+		}
+		return kayaBaseService;
+	}
+	
+	public static KayaBaseService createKayaService(Paramater paramater){
+		KayaBaseService kayaBaseService=null; //定义一个类
+		try {
+			String className = paramater.getServicename();
+			kayaBaseService = (KayaBaseService)Class.forName(className).newInstance(); //TODO:Packagepath 需要在参数中动态提取
+			kayaBaseService.setParamater(paramater);
+			kayaBaseService.operate();
+			
+		} catch (InstantiationException e) {//必须指定服务类
+			kayaLoger.error("The service class must be specified!");
+		} catch (IllegalAccessException e) { //服务类定义错误
+
+			kayaLoger.error("The service class is incorrectly defined!");
+		} catch (ClassNotFoundException e) { //找不到服务类
+			kayaLoger.error("The service class could not be found!");
+		}
+		return kayaBaseService;
+	}
+	
+//	public static KayaBaseService createKayaService(String servicename, ArrayList<Paramater> paramaterList){
+//		KayaBaseService kayaBaseService=null; //定义一个类
+//		try {
+//
+//			kayaBaseService = (KayaBaseService)Class.forName(servicename).newInstance(); //TODO:Packagepath 需要在参数中动态提取
+//			kayaBaseService.setParamaterList(paramaterList);
+//			kayaBaseService.operate();
+//			
+//		} catch (InstantiationException e) {//必须指定服务类
+//			kayaLoger.error("The service class must be specified!");
+//		} catch (IllegalAccessException e) { //服务类定义错误
+//
+//			kayaLoger.error("The service class is incorrectly defined!");
+//		} catch (ClassNotFoundException e) { //找不到服务类
+//			kayaLoger.error("The service class could not be found!");
+//		}
+//		return kayaBaseService;
+//	}
 }
