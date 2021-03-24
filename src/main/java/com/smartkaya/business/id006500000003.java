955c82b76e7afabb7c1c7b7af7a3342199e30694
LiangChen <LiangChen@DESKTOP-8ED6TEN.mshome.net>
2021-03-24 09:55:53 +0800

    update

--- src/main/java/com/smartkaya/business/id006500000003.java
+++ src/main/java/com/smartkaya/business/id006500000003.java
@@ -1,27 +1,30 @@
 package com.smartkaya.business;
 
 import com.smartkaya.bean.Paramaters;
+import com.smartkaya.service.BusinessAnnotation;
 import com.smartkaya.service.KayaBaseService;
 
 @BusinessAnnotation(maxMoney = 15000,objectId = "")
 public class id006500000003 extends KayaBaseService {
+
 
 	@Override
 	public void before() {
 		// TODO Auto-generated method stub
 		Paramaters paramaters = super.getParamaters();
 		paramaters.getId();
-		System.out.println("before");
 		
+		System.out.println(paramaters.getListPropertys().get(0).get("PhoneNumber"));
+		paramaters.getListPropertys().get(0).put("PhoneNumber", "66666666");
 	}
 
 //	@Override
-	public void excutBusiness() {
-		// TODO Auto-generated method stub
-		Paramaters paramaters = super.getParamaters();
-		paramaters.getId();
-		System.out.println("excutBusiness");
-	}
+//	public void excutBusiness() {
+//		// TODO Auto-generated method stub
+//		Paramaters paramaters = super.getParamaters();
+//		paramaters.getId();
+//		System.out.println("excutBusiness");
+//	}
 
 	@Override
 	public void after() {
