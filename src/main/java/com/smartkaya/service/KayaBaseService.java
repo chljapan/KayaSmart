150b53f4d48324bf4c5ae4e59be62ff6cbab6a45
LiangChen <LiangChen@DESKTOP-8ED6TEN.mshome.net>
2021-03-24 09:54:34 +0800

    update

--- src/main/java/com/smartkaya/service/KayaBaseService.java
+++ src/main/java/com/smartkaya/service/KayaBaseService.java
@@ -5,6 +5,9 @@ import com.smartkaya.bean.Paramaters;
 public abstract class KayaBaseService {
 	private Paramaters paramaters;
 	//这个参数是各个基本方法执行的顺序
+	public static KayaBaseService excuteService(Paramaters paramaters) {
+		return KayaFactory.createKayaService(paramaters);
+	}
 	
 	public void setParamaters(Paramaters paramaters) {
 		this.paramaters = paramaters;
--- src/main/java/com/smartkaya/service/KayaFactory.java
+++ src/main/java/com/smartkaya/service/KayaFactory.java
@@ -1,25 +1,26 @@
 package com.smartkaya.service;
 
 import com.smartkaya.bean.Paramaters;
+import com.smartkaya.log.KayaLogManager;
 
 public class KayaFactory {
 	//private static HashMap<String,KayaBaseService> KayaBaseServices = new HashMap<String,KayaBaseService>();
-	
-	//定一个烤箱，泥巴塞进去，人就出来，这个太先进了
+	private static KayaLogManager kayaLoger = KayaLogManager.getInstance();
 	public static KayaBaseService createKayaService(Paramaters paramaters){
-		KayaBaseService kayaBaseService=null; //定义一个类型的人类
+		KayaBaseService kayaBaseService=null; //定义一个类
 		try {
 			String className = paramaters.getId().replace("-", "");
-			kayaBaseService = (KayaBaseService)Class.forName("com.smartkaya.business." + className).newInstance(); //产生一个人类
+			kayaBaseService = (KayaBaseService)Class.forName("com.smartkaya.business." + className).newInstance(); //TODO:Packagepath 需要在参数中动态提取
 			kayaBaseService.setParamaters(paramaters);
+			kayaBaseService.operate();
 			
-		} catch (InstantiationException e) {//你要是不说个人类颜色的话，没法烤，要白的黑，你说话了才好烤
-			System.out.println("必须指定服务类");
-		} catch (IllegalAccessException e) { //定义的人类有问题，那就烤不出来了，这是...
+		} catch (InstantiationException e) {//必须指定服务类
+			kayaLoger.error("The service class must be specified!");
+		} catch (IllegalAccessException e) { //服务类定义错误
 
-			System.out.println("服务类定义错误！");
-		} catch (ClassNotFoundException e) { //你随便说个人类，我到哪里给你制造去？！
-			System.out.println("找不到服务类！");
+			kayaLoger.error("The service class is incorrectly defined!");
+		} catch (ClassNotFoundException e) { //找不到服务类
+			kayaLoger.error("The service class could not be found!");
 		}
 		return kayaBaseService;
 	}
