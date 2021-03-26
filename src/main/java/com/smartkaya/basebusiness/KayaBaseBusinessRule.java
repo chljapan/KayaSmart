940a70208ea08837b4c82cf31603a2044511c252
LiangChen <LiangChen@DESKTOP-8ED6TEN.mshome.net>
2021-03-26 18:56:15 +0800

    update

--- src/main/java/com/smartkaya/basebusiness/KayaBaseBusinessRule.java
+++ src/main/java/com/smartkaya/basebusiness/KayaBaseBusinessRule.java
@@ -1,55 +1,34 @@
 package com.smartkaya.basebusiness;
 
-import java.util.HashMap;
-
 import com.smartkaya.service.KayaBaseService;
 
 public class KayaBaseBusinessRule  extends KayaBaseService{
-	class emlInfo {
-		private  HashMap<String,Object> property;
-		emlInfo(HashMap<String,Object> property) {
-			this.property = property;
-		}
-		
-		public String 姓名 = (String) this.property.get("name");
-	}
 	
-	
-	
-	public KayaBaseBusinessRule() {
-		// TODO Auto-generated constructor stub
-	}
-
-
+	/**
+	 * 业务数据库操作前处理
+	 */
 	@Override
 	public void before() {
 		// TODO Auto-generated method stub
-		emlInfo 员工信息 = new emlInfo(this.getParamater().getPropertys());
-		
-		if( 员工信息.姓名 == "") {
-			员工信息.姓名 ="xxxxx";
-			
-		}
-		System.out.println("Base excutBusiness!");
-//		this.getParamater().getPropertys(){
-//			
-//		}
-//		.get(员工信息.姓名)
-//		.get("")
-//
-		
-	}
-
-	public void excutBusiness(){
-		System.out.println("Base excutBusiness!");
-
-		
+		kayaLoger.info("before");
 	}
 	
+	/**
+	 * 业务数据库操作 
+	 */
+	public void excutBusiness(){
+		this.kayaLoger.info("excutBusiness");
+		dao.execute(this.getParamater());
+			// 查询情况下返回查询结果
+	}
+	
+	/**
+	 * 业务数据库操作后处理
+	 */
 	@Override
 	public void after() {
 		// TODO Auto-generated method stub
-		
+		this.kayaLoger.info("after");
 	}
 
 }
